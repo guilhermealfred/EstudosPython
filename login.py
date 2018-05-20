206fc7305abe7f8a534f4f4a20013c9f7e27bff1
guilhermealfred <38086570+guilhermealfred@users.noreply.github.com>
2018-05-20 01:20:38 -0300

    Create login.py
    Minha primeira "GUI" criada em python.
    Sistema simples de login com funções de armazenamento de dados usando o módulo 'shelve'.

---
+++ login.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from tkinter import *
+import shelve
+class program:
+	def __init__(self,app):
+		self.log = Label(app,text='Login')
+		self.log.pack()
+		self.user = Entry(app)
+		self.user.pack()
+		self.sen = Label(app,text='Senha')
+		self.sen.pack()
+		self.senha = Entry(app,show='*')
+		self.senha.pack()
+		self.entra = Button(app,text='Entrar',command=self.login)
+		self.entra.pack(side= LEFT)
+		self.novo = Button(app,text='Novo',command=self.novo)
+		self.novo.pack(side=RIGHT)
+	def criar(self):
+		with shelve.open('users.db') as users:
+			if len(self.user.get()) > 0 and len(self.senha.get()) >0:
+				if self.user.get() not in users['user']:
+					users['user'] = self.user.get()
+					users['senha'] = self.senha.get()
+				else:
+					self.result = Label(app,text='Usuário já existe',fg='red')
+					self.result.pack()
+			else:
+				self.result = Label(app,text='Preencha todos campos')
+				self.result.pack()
+	def novo(self):
+		self.log['fg'] = 'green'
+		self.sen['fg'] = 'green'
+		self.novo['text'] = 'Criar'
+		self.novo['command'] = self.criar
+	def login(self):
+		with shelve.open('users.db') as users:
+			if len(self.user.get()) > 0 and len(self.senha.get()) >0:
+				if self.user.get() in users['user'] and self.senha.get() in users['senha']:
+					self.result= Label(app,text=f'Bem vindo {self.user.get()}',fg='blue')
+					self.result.pack()
+				else:
+					self.result = Label(app,text='Acesso negado',fg='red')
+					self.result.pack()
+			else:
+				self.result = Label(app,text='Preencha todos os campos',fg='red')
+				self.result.pack()
+app = Tk()
+app.geometry('200x200')
+app.title('Login')
+program(app)
+app.mainloop()
