9446882aa19e1a84de6cdf3817ae13a837fa9149
guilhermealfred <38086570+guilhermealfred@users.noreply.github.com>
2018-06-27 19:02:11 -0300

    Update substituicoes.py
    Continuação dos estudos de manipulação de arquivos '.json' com a adição de um método à classe(worldcupinfo) que faz o levantamento das substituições efetuadas nos jogos da copa do mundo de 2018.

--- substituicoes.py
+++ substituicoes.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import json
 import requests
 import csv
@@ -11,17 +10,18 @@ class worldcupinfo:
 	def playersinfo(self):
 		with open('titulares.csv','w+') as f:
 			writer = csv.writer(f)
-			x = []
 			for i in self.data:
 				if i['status'] == 'completed':
-					if i['home_team_statistics']['country'].upper() not in x:
-						writer.writerow(('','','%s'%(i['home_team_statistics']['country'].upper())))
-						x.append(i['home_team_statistics']['country'].upper())
-						writer.writerow(('NAME','POSITION','SHIRT-NUMBER'))
-						for s in i['home_team_statistics']['starting_eleven']:
-							writer.writerow((f"{s['name']}",f"{s['position']}",f"{s['shirt_number']}"))
-					else:
-						pass
+					writer.writerow(('','','%s x %s'%(i['home_team_country'],i['away_team_country']),'\n'))
+					writer.writerow(('','%s'%(i['home_team_statistics']['country'].upper()),'\n'))
+					writer.writerow(('NAME','POSITION','SHIRT-NUMBER'))
+					for s in i['home_team_statistics']['starting_eleven']:
+						writer.writerow((f"{s['name']}",f"{s['position']}",f"{s['shirt_number']}"))
+					writer.writerow(('','%s'%(i['away_team_statistics']['country'].upper()),'\n'))
+					for s in i['away_team_statistics']['starting_eleven']:
+						writer.writerow((f"{s['name']}",f"{s['position']}",f"{s['shirt_number']}"))
+				else:
+					pass
 
 	def substitutions(self):
 		with open('substitutions.csv','w') as f:
