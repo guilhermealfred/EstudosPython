821add6b07dd60605a722ac3cc2329fd59152d7b
guilhermealfred <38086570+guilhermealfred@users.noreply.github.com>
2018-06-27 19:01:20 -0300

    Update titulares.py
    Estudo de manipulação de arquivos '.json'. O código faz o levantamento dos 11 titulares de cada seleção em suas respectivas atuações na copa do mundo 2018.

--- titulares.py
+++ titulares.py
@@ -7,18 +7,19 @@ class worldcupinfo:
 		self.req = requests.get('http://worldcup.sfg.io/matches')
 		self.data = json.loads(self.req.content)
 	def playersinfo(self):
-		with open('worldcup.csv','w+') as f:
+		with open('titulares.csv','w+') as f:
 			writer = csv.writer(f)
-			x = []
 			for i in self.data:
 				if i['status'] == 'completed':
-					if i['home_team_statistics']['country'].upper() not in x:
-						writer.writerow(('','','%s'%(i['home_team_statistics']['country'].upper())))
-						x.append(i['home_team_statistics']['country'].upper())
-						writer.writerow(('NAME','POSITION','SHIRT-NUMBER'))
-						for s in i['home_team_statistics']['starting_eleven']:
-							writer.writerow((f"{s['name']}",f"{s['position']}",f"{s['shirt_number']}"))
-					else:
-						pass
+					writer.writerow(('','','%s x %s'%(i['home_team_country'],i['away_team_country']),'\n'))
+					writer.writerow(('','%s'%(i['home_team_statistics']['country'].upper()),'\n'))
+					writer.writerow(('NAME','POSITION','SHIRT-NUMBER'))
+					for s in i['home_team_statistics']['starting_eleven']:
+						writer.writerow((f"{s['name']}",f"{s['position']}",f"{s['shirt_number']}"))
+					writer.writerow(('','%s'%(i['away_team_statistics']['country'].upper()),'\n'))
+					for s in i['away_team_statistics']['starting_eleven']:
+						writer.writerow((f"{s['name']}",f"{s['position']}",f"{s['shirt_number']}"))
+				else:
+					pass
 x = worldcupinfo()
 x.playersinfo()
